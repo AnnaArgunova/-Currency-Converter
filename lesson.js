1313b1dac2cd802113e1abcd9a7e886472b9b128
Anna <anna.argunova@gmail.com>
2019-03-19 21:09:25 +0200

    Ajax+Promis

--- lesson.js
+++ lesson.js
@@ -2,19 +2,41 @@ let uan= document.getElementById('UAN'),
     inusd = document.getElementById('USD');
 
 uan.addEventListener('input', () => {
+
+    function catchData() {
+
+        return new Promise(function(resolve, reject){
+
     let request = new XMLHttpRequest();
 
     request.open('GET', 'lesson.json');
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     request.send(); 
 
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
+    request.onload = function() {
 
-            inusd.value = uan.value / data.usd;
-        } else {
-            inusd.value = "Что-то пошло не так!!!";
-        }
-    });
+        if(request.readyState === 4) {
+            if(request.status == 200) {
+                resolve(this.response)
+            }
+            else {
+                reject();
+            
+            }
+    }
+}
 });
+};
+
+catchData()
+.then(response => {
+console.log(response);
+        let data = JSON.parse(response);
+        inusd.value = uan.value / data.usd;
+})
+
+.then(() => console.log(5000))
+.catch(() => inusd.value = "Что-то пошло не так!!!")
+
+});
+    
